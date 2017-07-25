b7457b76ac52bcb91161415d6227fab9d935a208
wodog <536505032@qq.com>
2017-07-25 23:44:21 +0800

    add parse

---
+++ parse.js
@@ -0,0 +1,98 @@
+let pos, str
+
+module.exports = function parse (jsonStr) {
+  pos = 0
+  str = jsonStr
+  return parseValue()
+}
+
+function parseValue () {
+  skip()
+  let result = ''
+  const char = str[pos]
+  if (char === '{') {
+    result = parseObject()
+  } else if (char === '[') {
+    result = parseArray()
+  } else if (char === '"') {
+    result = parseString()
+  } else if (char === 't') {
+    result = parseTrue()
+  } else if (char === 'f') {
+    result = parseFalse()
+  } else if (char === 'n') {
+    result = parseNull()
+  } else if (isNumber(char)){
+    result = parseNumber()
+  } else {
+    throw new Error('[JSON parse error]: syntax error')
+  }
+  skip()
+  return result
+}
+
+function parseObject() {
+  const result = {}
+  pos++
+  while (str[pos] !== '}') {
+    const key = parseValue()
+    const value = parseValue()
+    result[key] = value
+  }
+  return result
+}
+
+function parseArray () {
+  const result = []
+  pos++
+  while (str[pos] !== ']') {
+    result.push(parseValue())
+    skip()
+  }
+  pos++
+  return result
+}
+
+function parseString() {
+  let result = ''
+  pos++
+  while (str[pos] !== '"') {
+    result += str[pos++]
+  }
+  pos++
+  return result
+}
+
+function parseTrue() {
+  pos += 4
+  return true
+}
+
+function parseFalse() {
+  pos += 5
+  return false
+}
+
+function parseNull() {
+  pos += 4
+  return null
+}
+
+function parseNumber() {
+  let result = ''
+  while (isNumber(str[pos])) {
+    result += str[pos++]
+  }
+  return parseInt(result)
+}
+
+
+function isNumber(char) {
+  return parseInt(char) > 0
+}
+
+function skip () {
+  while (str[pos] === ' ' || str[pos] === ':' || str[pos] === ',' || str[pos] === '\n' || str[pos] === '\r') {
+    pos++
+  }
+}
